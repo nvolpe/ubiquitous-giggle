130533040b2a538aaf077345e295dbb7e79cd139
Nick Volpe <nvolpe@dtsagile.com>
2018-02-21 20:59:11 -0700

    initial checkin

---
+++ GuessingGame/GuessingGame/GameViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+
+using CoreFoundation;
+using UIKit;
+using Foundation;
+using GuessingGame.Services;
+
+namespace GuessingGame
+{
+    [Register("UniversalView")]
+    public class UniversalView : UIView
+    {
+        public UniversalView()
+        {
+            Initialize();
+        }
+
+        public UniversalView(RectangleF bounds) : base(bounds)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            BackgroundColor = UIColor.Red;
+        }
+    }
+
+    [Register("GameViewController")]
+    public class GameViewController : UIViewController
+    {
+        public GameViewController()
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            View = new UniversalView();
+
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view
+
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            PlayerDataService playerService = new PlayerDataService();
+            playerService.GetPlayers();
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Services/PlayerDataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GuessingGame.Services
+{
+    class PlayerDataService
+    {
+        public async void GetPlayers()
+        {
+            try
+            {
+                {
+                    using (var client = new HttpClient())
+                    {
+                        // GET the player data url
+                        HttpResponseMessage response = await client.GetAsync("https://cdn.rawgit.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json");
+                        var contentReponse = response.Content.ReadAsStringAsync().Result;
+
+                        //if (response.IsSuccessStatusCode)
+                        //{
+                        //}
+                        //else if (response.StatusCode == HttpStatusCode.BadRequest)
+                        //{
+                        //}
+                    }
+                }
+
+            }
+
+            //catch (HttpRequestException exc)
+            //{
+            //}
+
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+
+    }
+}
